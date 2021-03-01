7fcec220989920369f19ab0d09edb50dead4aabb
Taylor Beck <beck.taylorg@gmail.com>
2021-02-28 20:09:08 -0800

    Create basic shop reducer using shop data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,17 +5,19 @@ import storage from 'redux-persist/lib/storage'; // localStorage
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whiteList: ['cart', 'directory'] // array of reducers to persist
+  whiteList: ['cart', 'directory', 'shop'] // array of reducers to persist
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
