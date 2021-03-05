c5d682993747827a2185bc898414ab00472f7b9a
Taylor Beck <beck.taylorg@gmail.com>
2021-03-04 16:28:18 -0800

    Refactor footer to use Link components

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './footer.styles.scss';
 
-const Footer = ({ history }) => {
+const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className='footer'>
       <div className='copyright'>
-        <p>&copy; {currentYear} Freedom Fit. All Rights Reserved.</p>
+        <span>&copy; {currentYear} Freedom Fit. All Rights Reserved.</span>
       </div>
       <div className='legal-links'>
-        <div className='link'>
-          <p onClick={() => history.push('/terms-of-service')}>Terms of Service</p>   
-        </div>
-        <div className='link'>
-          <p onClick={() => history.push('/privacy-policy')}>Privacy Policy</p>
-        </div>
-        <div className='link'>
-          <p>Shipping Policy</p>
-        </div>
-        <div className='link'>
-          <p onClick={() => history.push('/return-refund-policy')}>Return & Refund Policy</p>
-        </div>
+        <Link className='link' to='/terms-of-service'>Terms of Service</Link>
+        <Link className='link' to='/privacy-policy'>Privacy Policy</Link>
+        <Link className='link' to='/shipping-policy'>Shipping Policy</Link>
+        <Link className='link' to='/return-refund-policy'>Return & Refund Policy</Link>
       </div>
     </div>
   );
 };
 
-export default withRouter(Footer);
+export default Footer;
