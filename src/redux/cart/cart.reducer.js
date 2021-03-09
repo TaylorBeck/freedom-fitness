1806259d3e8e4116bbd1881ed1fcd26af553a388
Taylor Beck <beck.taylorg@gmail.com>
2021-03-08 18:20:39 -0800

    Add redux action to clear all items from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -22,12 +22,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: removeItemFromCart(state.cartItems, action.payload)
-      }
+      };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
         cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
       };
+    case CartActionTypes.CLEAR_ALL_ITEMS_FROM_CART:
+      return INITIAL_STATE;
     default:
       return state;
   }
