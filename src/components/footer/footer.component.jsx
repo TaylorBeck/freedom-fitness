81c3e2f872bcddcbfd42eb308bab5bd509c54bde
Taylor Beck <beck.taylorg@gmail.com>
2021-03-08 19:51:32 -0800

    Add Github repo link to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as GithubLogo } from '../../assets/github.svg';
+
 import './footer.styles.scss';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const githubLink = 'https://github.com/TaylorBeck/freedom-fitness';
   return (
     <div className='footer'>
       <div className='copyright'>
         <span>&copy; {currentYear} Freedom Fit. All Rights Reserved.</span>
+      </div>
+      <div className='socials-container'>
+        <a className='github-logo' href={githubLink}><GithubLogo/></a>
       </div>
       <div className='legal-links'>
         <Link className='link' to='/terms-of-service'>Terms of Service</Link>
