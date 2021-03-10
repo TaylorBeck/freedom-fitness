f3c9e0261d9c369ef4668dc8d450fceb81ae0300
Taylor Beck <beck.taylorg@gmail.com>
2021-03-09 19:02:23 -0800

    Use promise pattern instead of observable onSnapshot for Firestore collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -22,13 +22,11 @@ class ShopPage extends React.Component {
     loading: true
   };
 
-  unsubscribeFromSnapshot = null;
-
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
 
