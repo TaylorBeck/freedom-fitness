2c36c445e174f552454e0fadc75b8a30f642f714
Taylor Beck <beck.taylorg@gmail.com>
2021-03-13 15:56:01 -0800

    Remove old async shop collection fetching and replace ShopPage action

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -18,26 +18,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-// Using thunk (redux middleware), GET shop collections or an error
-export const fetchCollectionsStartAsync = () => {
-  // redux-thunk middleware will call dispatched functions with the `dispatch` method as the first argument
-  return dispatch => {
-    // GET the shop `collections` collection reference
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart());
-
-    // GET the snapshot (the actual data) of the collectionRef
-    collectionRef
-      .get()
-      .then(snapshot => {
-        // SUCCESS
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch(error => {
-        // FAILURE
-        dispatch(fetchCollectionsFailure(error.message));
-      });
-  };
-};
