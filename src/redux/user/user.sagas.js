0ba344fb868f2f7e5267d926b0154072f9b6bc57
Taylor Beck <beck.taylorg@gmail.com>
2021-03-14 14:48:49 -0700

    Reimplement sign out as a user saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -4,7 +4,9 @@ import UserActionTypes from './user.types';
 
 import {
   signInSuccess,
-  signInFailure
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 } from './user.actions';
 
 import {
@@ -36,16 +38,6 @@ export function* signInWithGoogle() {
   }
 }
 
-export function* isUserAuthenticated() {
-  try {
-    const userAuth = yield getCurrentUser();
-    if (!userAuth) return; // userAuth is null
-    yield getSnapshotFromUserAuth(userAuth);
-  } catch(error) {
-    yield put(signInFailure(error));
-  }
-}
-
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -63,6 +55,29 @@ export function* onEmailSignInStart() {
   yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield put(signOutSuccess());
+  } catch(error) {
+    yield put(signOutFailure(error));
+  }
+}
+
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
+export function* isUserAuthenticated() {
+  try {
+    const userAuth = yield getCurrentUser();
+    if (!userAuth) return; // userAuth is null
+    yield getSnapshotFromUserAuth(userAuth);
+  } catch(error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
@@ -71,6 +86,7 @@ export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
-    call(onCheckUserSession)
+    call(onCheckUserSession),
+    call(onSignOutStart)
   ]);
 }
