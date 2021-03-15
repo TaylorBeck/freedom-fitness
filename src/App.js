17124cc3a038306a03d7f04615d31ce2e3f20676
Taylor Beck <beck.taylorg@gmail.com>
2021-03-14 21:26:53 -0700

    Add media queries and overall styling for mobile using styled components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import GlobalStyle from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -32,6 +32,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
